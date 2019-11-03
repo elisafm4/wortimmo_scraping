9e2688006c699a713cf32c1af54cad18686a3ba4
elisafm4 <44262088+elisafm4@users.noreply.github.com>
2019-11-03 22:04:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# libraries needed
+from bs4 import BeautifulSoup
+from requests import get
+import pandas as pd
+import itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
+from time import sleep
+import random
