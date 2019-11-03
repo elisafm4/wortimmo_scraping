0628963f9dde8aa89798fdcbc135344fc2e5ae47
elisafm4 <44262088+elisafm4@users.noreply.github.com>
2019-11-04 00:25:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # libraries needed
 from bs4 import BeautifulSoup
-from requests import get
-import pandas as pd
+import requests
+import urllib.request
+import time
 import itertools
-import matplotlib.pyplot as plt
-import seaborn as sns
-from time import sleep
+import pandas as pd
 import random
+from auxiliar_func import search_by, get_data
+
+headers = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+# set the filters to apply
+wortimmo = search_by(transaction_type='vente', location='country-1', property_type=0, min_beds=2)
+# get url
+response = requests.get(wortimmo, headers=headers)
+html_page = BeautifulSoup(response.text, 'html.parser')
+
+# get dataframe and convert into xls
+df = get_data(html_page,5)
+df.to_excel('luxemburgo_raw.xls')
