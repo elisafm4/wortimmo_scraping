04c5e563ee3aad59a4615c3a7ace89b3a4c4dcfc
elisafm4 <44262088+elisafm4@users.noreply.github.com>
2019-11-05 20:51:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import itertools
 import pandas as pd
 import random
 from auxiliar_func import search_by, get_data
+from datetime import datetime
 
 headers = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
@@ -17,5 +18,11 @@ response = requests.get(wortimmo, headers=headers)
 html_page = BeautifulSoup(response.text, 'html.parser')
 
 # get dataframe and convert into xls
-df = get_data(html_page,5)
-df.to_excel('luxemburgo_raw.xls')
+num = 5 # last 5 adverts
+# get date
+now = datetime.now() # current date and time
+num=5
+date_time = now.strftime("%m_%d_%Y")
+name_excel = str(date_time+'_'+str(num)+'Adverts.xls')
+df = get_data(html_page,num)
+df.to_excel(name_excel)
